64d28e57f3f5b8a6ab7194fa59e6e1cc43e873d3
Praveen H Hugar <praveenhugar1002@gmail.com>
2026-02-22 18:02:31 +0530

    Program for linear search in an array of n elements

---
+++ lin_search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,position,key;
+    cout<<"Enter n value"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter key element"<<endl;
+    cin>>key;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==key)
+        {
+            position=i;
+            break;
+        }
+    }
+    if(position==-1)
+    {
+        cout<<"Search is unsecessful"<<endl;
+    }
+    else{
+        cout<<"Elements found at"<<position<<""<<endl;
+    }
+    return 0;
+}
